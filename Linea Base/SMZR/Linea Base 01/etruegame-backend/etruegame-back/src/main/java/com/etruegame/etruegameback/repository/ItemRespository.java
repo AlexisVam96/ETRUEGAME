93dbf329acf53fe3aa0c74e651d6b7e3b4005e29
AlexisVam96 <66218720+AlexisVam96@users.noreply.github.com>
2022-01-21 11:11:16 -0500

    Delete Linea Base/SMZR/Linea Base 01 directory

--- Linea Base/SMZR/Linea Base 01/etruegame-backend/etruegame-back/src/main/java/com/etruegame/etruegameback/repository/ItemRespository.java
+++
@@ -1,7 +0,0 @@
-package com.etruegame.etruegameback.repository;
-
-import com.etruegame.etruegameback.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRespository extends JpaRepository<Item, Integer> {
-}
