576c542cbd643bc8674c8cce906f6f24df2e3861
AlexisVam96 <alexispajuelo6@gmail.com>
2022-01-21 11:02:27 -0500

    subiendo entregable de linea base 2

---
+++ Linea-Base/SMZR/Linea Base 01/etruegame-backend/etruegame-back/src/test/java/com/etruegame/etruegameback/EtruegameBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etruegame.etruegameback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtruegameBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Linea-Base/SMZR/Linea Base 02/etruegame-back/src/main/java/com/etruegame/etruegameback/EtruegameBackApplication.java
@@ -0,0 +1,13 @@
+package com.etruegame.etruegameback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtruegameBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtruegameBackApplication.class, args);
+	}
+
+}
